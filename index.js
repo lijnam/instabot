49e38c9d4e260d18e1a32dbccfb367bd5afb3941
Manjil <mshrst16@gmail.com>
2020-01-28 00:22:30 +0545

    fix(index.js): added more log message and unfollow logics

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ const USERNAME = '';
 /* instagram password */
 const PASSWORD = '';
 
-var total_people_folowed = 0;
+var total_people_followed = 0;
+var total_people_unfollowed = 0;
+var oneSecond = 1000;
+var oneMinute = oneSecond * 60;
+var oneHour = oneMinute * 60;
+
 
 function randomRange(min, max) {
     return ~~(Math.random() * (max - min + 1)) + min
@@ -60,6 +65,7 @@ async function scrapeInfiniteScrollItems(
 
 async function init() {
     try {
+        log('opening Browser...');
         const browser = await puppeteer.launch({
             // headless: false,
             // executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
@@ -91,6 +97,7 @@ function getDateWithTimeAddition(date, miliSeconds) {
 
 async function gotoInstaLoginPage() {
     try {
+        log('going to login page');
         await page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher',
             { waitUntil: 'networkidle2' });
         return true;
@@ -103,18 +110,22 @@ async function gotoInstaLoginPage() {
 }
 
 async function submitLoginForm() {
+
+    log('inputing username')
     /* username input */
     await page.waitFor('._2hvTZ.pexuQ.zyHYP[name="username"]');
     await page.type('._2hvTZ.pexuQ.zyHYP[name="username"]', USERNAME, { delay: 200 });
     // await page.type('._2hvTZ.pexuQ.zyHYP[name="username"]', 'qrstorenepal', { delay: 200 });
 
 
+    log('inputing password');
     /* password input */
     await page.waitFor('._2hvTZ.pexuQ.zyHYP[name="password"]');
     await page.type('._2hvTZ.pexuQ.zyHYP[name="password"]', PASSWORD, { delay: 200 });
     // await page.type('._2hvTZ.pexuQ.zyHYP[name="password"]', 'welcome$1290$', { delay: 200 });
 
 
+    log('logging in...')
     /* click submit */
     await page.click('button.sqdOP.L3NKy.y3zKF');
 }
@@ -122,8 +133,12 @@ async function submitLoginForm() {
 async function verificationCodes(params) {
     try {
         /* input backup code */
-        await page.waitFor('._2hvTZ.pexuQ.zyHYP[name="verificationCode"]');
+        try {
+            await page.waitFor('._2hvTZ.pexuQ.zyHYP[name="verificationCode"]');
+        } catch (error) { }
+
         if (await page.$('._2hvTZ.pexuQ.zyHYP[name="verificationCode"]') !== null) {
+            log('inputing backup codes for 2 factor verification')
             await page.type('._2hvTZ.pexuQ.zyHYP[name="verificationCode"]', BACKUP_CODES[0], { delay: 100 });
             /* click verify */
             await page.click('button.sqdOP.L3NKy.y3zKF');
@@ -140,6 +155,8 @@ async function removeNotification() {
         if (await page.$('button.aOOlW.HoLwm') !== null) {
             /* click "not now" for push notification */
             await page.click('button.aOOlW.HoLwm');
+            log('"Turn on Notifications"  popup removed. clicked "Not now"')
+
         }
         return true;
 
@@ -177,6 +194,7 @@ async function likePosts(page) {
             // let extractedElements = await page.$$(LIKEBTNS);
 
             /* Scroll and extract items from the page.  */
+            log('scrolling some posts..')
             await scrapeInfiniteScrollItems(page, LIKEBTNS, 50);
 
             posts = await page.$$eval(LIKEBTNS, async (svgs) => {
@@ -205,20 +223,18 @@ async function likePosts(page) {
             randomRange = function (min, max) {
                 return ~~(Math.random() * (max - min + 1)) + min
             }
-            let oneSecond = 1000;
-            let oneMinute = oneSecond * 60;
-            let oneHour = oneMinute * 60;
+
 
             /* wait for some time if likeable post is 0 */
             if (posts.likeable_posts < 1) {
-                log("Following more people..");
+                log("Following more accounts..");
 
-                /* follow more people */
+                /* follow more account */
                 followed_people = await followPeople(page);
-                log(followed_people + " new People followed ");
 
-                /* reset total people followed  */
-                total_people_folowed = 0;
+
+                /* reset total account followed  */
+                total_people_followed = 0;
 
                 /* wait for 6 hours */
                 log("waiting  for 6 hours no more likeable posts...")
@@ -226,20 +242,16 @@ async function likePosts(page) {
                 await page.waitFor(oneHour * 6); // 
                 // await page.waitFor(30000);
             } else {
-                if (total_people_folowed <= 100) {
-                    log("Total followed people :" + total_people_folowed);
-                    log("Following more people...");
+                if (total_people_followed <= 100) {
+                    log("Total followed account :" + total_people_followed);
+                    log("Following more account...");
 
-                    /* follow more people */
+                    /* follow more account */
                     followed_people = await followPeople(page);
-                    log(followed_people + " new People followed ");
 
-                    /* wait some time  */
-                    waitTime = randomRange(30, 60);
-                    log("waiting for " + waitTime + " minutes after following people");
-                    log('will resume at ' + getDateWithTimeAddition(new Date(), oneMinute * waitTime));
 
-                    await page.waitFor(oneMinute * waitTime);
+                    /* try to unfollow some account */
+                    await unfollowPeople();
                 }
                 /* wait for random minutes  */
                 waitTime = randomRange(30, 120);
@@ -262,6 +274,7 @@ async function followPeople(page) {
     try {
         await page.waitFor(randomRange(5000, 10000));
 
+        log('going to suggested people page')
         await page.goto('https://www.instagram.com/explore/people/suggested/',
             { waitUntil: 'networkidle2' });
 
@@ -278,36 +291,134 @@ async function followPeople(page) {
                 return ~~(Math.random() * (max - min + 1)) + min
             }
             let no_of_people_to_follow = 5;
-            let total_people_folowed = 0
+            let total_people_followed = 0
             for (let follow_button of follow_buttons) {
-                // console.log('total_people_folowed : ' + total_people_folowed);
+                // console.log('total_people_followed : ' + total_people_followed);
                 // console.log('no_of_people_to_follow : ' + no_of_people_to_follow);
 
-                if (total_people_folowed > no_of_people_to_follow) {
-                    // console.log(total_people_folowed)
+                if (total_people_followed > no_of_people_to_follow) {
+                    // console.log(total_people_followed)
                     break;
                 }
                 // wait 
                 await new Promise(function (resolve) { setTimeout(resolve, randomRange(3000, 10000)) });
                 // console.log(follow_button);
                 await follow_button.click();
-                total_people_folowed++;
+                total_people_followed++;
 
             }
             return {
-                total_people_folowed: total_people_folowed,
+                total_people_followed: total_people_followed,
                 followable_people: follow_buttons.length
             };
         });
-        total_people_folowed += follow_object.total_people_folowed;
+        total_people_followed += follow_object.total_people_followed;
+
+        /* wait some time  */
+        waitTime = randomRange(30, 60);
+        log("waiting for " + waitTime + " minutes after following account");
+        log('will resume at ' + getDateWithTimeAddition(new Date(), oneMinute * waitTime));
+
+        await page.waitFor(oneMinute * waitTime);
 
         await page.waitFor(randomRange(5000, 10000));
+        log('going to instagram homepage');
         await page.goto('https://www.instagram.com', { waitUntil: 'networkidle2' });
         await page.waitFor(randomRange(5000, 30000));
-        return follow_object.total_people_folowed;
+
+        log(follow_object.total_people_followed + " new accounts followed ");
+
+
+        return follow_object.total_people_followed;
 
     } catch (error) {
         logError('on followPeople', error);
+        log('going to instagram homepage');
+        await page.goto('https://www.instagram.com', { waitUntil: 'networkidle2' });
+        return false;
+    }
+}
+
+async function unfollowPeople(page) {
+    try {
+        await page.waitFor(randomRange(5000, 10000));
+
+        log('going to profile page...')
+        await page.goto('https://www.instagram.com/' + USERNAME,
+            { waitUntil: 'networkidle2' });
+
+        /* chcek number of followers */
+        total_followers_selector = '.Y8-fY .-nal3[href="/qrstorenepal/followers/"] span.g47SY';
+        await page.waitFor(total_followers_selector);
+        total_followers = await page.$eval(total_followers_selector, e => e.innerText);
+        log('number of followers : ' + total_followers);
+
+        /* check nubmber of followed users */
+        total_following_selector = '.Y8-fY .-nal3[href="/qrstorenepal/following/"] span.g47SY';
+        await page.waitFor(total_following_selector);
+        total_following = await page.$eval(total_following_selector, e => e.innerText);
+        log('nubmber of followed account  : ' + total_following);
+
+        if (parseInt(total_following) < 5000) {
+            throw ('Wont start unfollowing until total number of followed account is greater than ' + 5000);
+        }
+
+        /* click following  */
+        following_selector = '.Y8-fY .-nal3[href="/qrstorenepal/following/"]';
+
+        await page.waitFor(following_selector);
+        await page.click(following_selector);
+
+        following_button_selector = '.Igw0E.rBNOH.YBx95.ybXk5._4EzTm.soMvl button.sqdOP.L3NKy._8A5w5'
+        await page.waitFor(following_button_selector);
+
+        unfollow_object = await page.$$eval(following_button_selector, async (following_buttona) => {
+
+            randomRange = function (min, max) {
+                return ~~(Math.random() * (max - min + 1)) + min
+            }
+            let no_of_people_to_unfollow = 5;
+            let total_people_unfollowed = 0
+            for (let following_button of following_buttona) {
+
+                if (total_people_unfollowed > no_of_people_to_unfollow) {
+                    break;
+                }
+
+                // wait 
+                await new Promise(function (resolve) { setTimeout(resolve, randomRange(3000, 10000)) });
+                await following_button.click();
+
+                /* wait for unfollow popup */
+                await new Promise(function (resolve) { setTimeout(resolve, randomRange(3000, 10000)) });
+                await document.querySelector('button.aOOlW.-Cab_').click();;
+
+                total_people_unfollowed++;
+
+            }
+            return {
+                total_people_unfollowed: total_people_unfollowed,
+                unfollowable_people: following_buttona.length
+            };
+        });
+        total_people_unfollowed += unfollow_object.total_people_unfollowed;
+
+        /* wait some time  */
+        waitTime = randomRange(30, 60);
+        log("waiting for " + waitTime + " minutes after unfollowing account");
+        log('will resume at ' + getDateWithTimeAddition(new Date(), oneMinute * waitTime));
+        await page.waitFor(oneMinute * waitTime);
+
+        await page.waitFor(randomRange(5000, 10000));
+        log('going to instagram homepage');
+        await page.goto('https://www.instagram.com', { waitUntil: 'networkidle2' });
+        await page.waitFor(randomRange(5000, 30000));
+        return unfollow_object.total_people_unfollowed;
+
+
+    } catch (error) {
+        logError('on unfollowPeople', error);
+        log('going to instagram homepage');
         await page.goto('https://www.instagram.com', { waitUntil: 'networkidle2' });
         return false;
     }
